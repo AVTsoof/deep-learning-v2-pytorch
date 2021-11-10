a1f7257a09f3f0bdeddb7089e1f4514a55bdc2e7
AVTsoof <tsoof@remote.com>
2021-11-10 18:56:20 +0200

    BEGIN (NOT DONE) bikesharing project

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -16,20 +16,11 @@ class NeuralNetwork(object):
                                        (self.hidden_nodes, self.output_nodes))
         self.lr = learning_rate
         
-        #### TODO: Set self.activation_function to your implemented sigmoid function ####
-        #
-        # Note: in Python, you can define a function with a lambda expression,
-        # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
+        #### Set self.activation_function to your implemented sigmoid function ####
+        self.activation_function = lambda x : 1/(1 + np.exp(-x))
+        # derivative of sigmoid function
+        self.activation_function_derivative = lambda x : self.activation_function(x) * (1 - self.activation_function(x))
         
-        ### If the lambda code above is not something you're familiar with,
-        # You can uncomment out the following three lines and put your 
-        # implementation there instead.
-        #
-        #def sigmoid(x):
-        #    return 0  # Replace 0 with your sigmoid calculation here
-        #self.activation_function = sigmoid
-                    
 
     def train(self, features, targets):
         ''' Train the network on batch of features and targets. 
@@ -45,9 +36,7 @@ class NeuralNetwork(object):
         delta_weights_i_h = np.zeros(self.weights_input_to_hidden.shape)
         delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
         for X, y in zip(features, targets):
-            
-            final_outputs, hidden_outputs = self.forward_pass_train(X)  # Implement the forward pass function below
-            # Implement the backproagation function below
+            final_outputs, hidden_outputs = self.forward_pass_train(X)
             delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y, 
                                                                         delta_weights_i_h, delta_weights_h_o)
         self.update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
@@ -61,15 +50,13 @@ class NeuralNetwork(object):
             X: features batch
 
         '''
-        #### Implement the forward pass here ####
-        ### Forward pass ###
-        # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        # Hidden layer
+        hidden_inputs = X.dot(self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
 
-        # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        # Output layer
+        final_inputs = hidden_outputs.dot(self.weights_hidden_to_output) # signals into final output layer
+        final_outputs = final_inputs # f(x)=x ; signals from final output layer
         
         return final_outputs, hidden_outputs
 
@@ -84,11 +71,12 @@ class NeuralNetwork(object):
             delta_weights_h_o: change in weights from hidden to output layers
 
         '''
-        #### Implement the backward pass here ####
-        ### Backward pass ###
-
-        # TODO: Output error - Replace this value with your calculations.
-        error = None # Output layer error is the difference between desired target and actual output.
+        ## AVTsoof: See (https://dustinstansbury.github.io/theclevermachine/derivation-backpropagation)
+        
+        
+        # Output error
+        # Output layer error is the difference between desired target and actual output.
+        error = y - final_outputs
         
         # TODO: Calculate the hidden layer's contribution to the error
         hidden_error = None
@@ -116,9 +104,11 @@ class NeuralNetwork(object):
 
         '''
         
-        # TODO: Update the weights with gradient descent step
-        self.weights_hidden_to_output += None # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += None # update input-to-hidden weights with gradient descent step
+        # Update the weights with gradient descent step
+        # update hidden-to-output weights with gradient descent step
+        self.weights_hidden_to_output -= delta_weights_h_o * self.lr
+        # update input-to-hidden weights with gradient descent step
+        self.weights_input_to_hidden -= delta_weights_i_h * self.lr
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
